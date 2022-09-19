93a19be575cc5032ce588fd6934eeda8e774dd45
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-09-18 20:28:25 -0500

    api route organization

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -5,18 +5,28 @@ const {
     getThoughtById, 
     createThought, 
     updateThought, 
-    deleteThought
+    deleteThought, 
+    createReaction,
+    deleteReaction
 } = require('../../controllers/thought-controller'); 
 
-
-router.route('/')
+// GET all and POST at /api/thoughts
+router
+  .route('/')
   .get(getAllThought)
   .post(createThought);
 
+// GET one, PUT, and DELETE at /api/thoughts/:thoughtid
 router
   .route('/:thoughtId')
   .get(getThoughtById)
   .put(updateThought)
   .delete(deleteThought);
+
+// POST and DELETE at /api/thoughts/:thoughtID/reactions
+router
+  .route('/:thoughtId/reactions')
+  .post(createReaction)
+  .delete(deleteReaction); 
 
 module.exports = router;
